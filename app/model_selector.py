25945195e7ddbcf54f8cacd694b9125236fb461b
Martyn Garcia <martyn.garcia@gmail.com>
2025-02-20 21:02:46 -0700

    add new models

--- app/model_selector.py
+++ app/model_selector.py
@@ -4,6 +4,8 @@ MODEL_NICKNAMES = {
     "o3-mini": "Ozone 3",
     "chatgpt-4o-latest": "GPT-4O Maestro",
     "deepseek-reasoner": "Deep Seeker",
+    "grok-3": "Grok Rhymes",
+    "r1-1776": "Perplexity Renegade",
     "claude-3-5-sonnet-20241022": "Sonnet 3.5",
     "gemini-2.0-pro-exp-02-05": "G-Mini Pro",
     "gemini-2.0-flash-thinking-exp-01-21": "G-Mini Flash"
