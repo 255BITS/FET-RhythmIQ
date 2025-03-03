1fad423537a6bfacb55d83082f8e4dc08372b198
Martyn Garcia <martyn.garcia@gmail.com>
2025-03-02 23:20:37 -0700

    wip audio

--- app/zonos_test.py
+++ app/zonos_test.py
@@ -1,1 +1,20 @@
+import os
 API_KEY=os.getenv("ZONOS_API_KEY", "zsk-d0f94527697f2e5edaaf7f12b127f41ac5879da89fa7364b18e10255b1903d10")
+
+from zyphra import ZyphraClient
+
+client = ZyphraClient(api_key=API_KEY)
+
+with ZyphraClient(api_key=API_KEY) as client:
+    # Save to file
+    output_path = client.audio.speech.create(
+        text="Hello, world!",
+        speaking_rate=15,
+        output_path="output.webm"
+    )
+    
+    # Get bytes
+    #audio_data = client.audio.speech.create(
+    #    text="Hello, world!",
+    #    speaking_rate=15
+    #)
