7bd4ddbe6476ad926ef4dec8e77f2c320afa56b1
Martyn Garcia <martyn.garcia@gmail.com>
2025-03-02 23:22:24 -0700

    add tag line

--- app/zonos_test.py
+++ app/zonos_test.py
@@ -8,7 +8,7 @@ client = ZyphraClient(api_key=API_KEY)
 with ZyphraClient(api_key=API_KEY) as client:
     # Save to file
     output_path = client.audio.speech.create(
-        text="Hello, world!",
+        text="You're listening to 97.3 the GOAT. A.I. Rap God 24 hours a day!",
         speaking_rate=15,
         output_path="output.webm"
     )
